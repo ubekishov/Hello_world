b28909ef87981ad975e5ba881f6a140066af8c29
ubekishov <ulan.bekishov@coveros.com>
2016-02-11 15:02:52 -0500

    2nd test removed

--- src/main/java/com/coveros/hello_world/EvenNumberGenerator.java
+++ src/main/java/com/coveros/hello_world/EvenNumberGenerator.java
@@ -15,7 +15,7 @@ public class EvenNumberGenerator {
 		Random randomGenerator = new Random();
 		int result = randomGenerator.nextInt();
 		
-		if (result%2!=1){
+		if (result%2!=0){
 			result++;
 		}
 		return result;
